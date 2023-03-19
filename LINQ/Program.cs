3d84c1488aeac5dd67618e7fd344c56251c4a465
ethan grossman <ethangrossman3@gmail.com>
2023-03-19 15:51:13 -0500

    finished exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace LINQ
+﻿using System.Runtime.Versioning;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            List<string> videoGames = new List<string>()
+            {
+                "DK 64",
+                "Ratchet and Clank",
+                "Call of Duty",
+                "Madden",
+                "NBA 2k",
+                "NBA Street",
+                "NFL Street",
+                "Assassins Creed",
+                "MarioKart",
+                "Tropico 6",
+                "Railway Empire",
+                "Sonic the Hedgehog",
+                "James Bond: Golden Eye",
+                "NCAA Football",
+                "PGA Tour 2k22",
+                "SIMS",
+
+            };
+            videoGames.Add("Fornite");
+
+            var orderedGames = videoGames.OrderBy(name => name.Length);
+
+            foreach (var order in orderedGames)
+            {
+                Console.WriteLine(order);
+            }
         }
     }
 }
